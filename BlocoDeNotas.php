167058babfb027a46da1df2309e31c1fd3babac6
Rodolfo <rodolfo_una@hotmail.com>
2020-04-12 23:52:45 -0300

    classes basicas do sistema foram criadas

---
+++ BlocoDeNotas.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BlocoDeNotas;
+
+class BlocoDeNotas{
+    private $Colaboradores;
+    private $Notas; //Array de notas. Blocos de informações categorizadas por assunto.
+    // private $;
+
+    public function adicionarColaborador($colaborador){
+
+    }
+
+    public function removerColaborador($colaborador){
+        
+    }
+
+    public function criarNota($colaborador){
+
+    }
+
+    public function alterarNota($colaborador){
+        
+    }
+
+    public function removerNota($colaborador){
+        
+    }
+
+}
---
+++ Nota.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace nota;
+
+class Nota{
+    private $categoria; //Enumeração para definir a categoria de informações que serão armazenadas;
+    private $informacao; //Array com informações que serão adicionadas, como textos ou links
+    // private $;
+}
